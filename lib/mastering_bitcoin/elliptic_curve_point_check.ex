84872b74518f58714619248d425ebb565bd6246b
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-10-24 12:10:34 +1100

    Add module that can confirm a point is on the elliptic curve, along with a custom function for exponential values in it

---
+++ lib/mastering_bitcoin/elliptic_curve_point_check.ex
@@ -0,0 +1,26 @@
+defmodule MasteringBitcoin.EllipticCurvePointCheck do
+  @moduledoc """
+  Example 4-1. Using Python to confirm that this point is on the elliptic curve.
+  """
+
+  require Integer
+
+  def run do
+    p = 115792089237316195423570985008687907853269984665640564039457584007908834671663
+    x = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+    y = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+    pow(x, 3) + 7 - pow(y, 2)
+    |> Integer.mod(p)
+  end
+
+  # NOTE: Elixir doesn't have an exponent function, and `:math.pow` in Erlang's
+  # library doesn't play nice with large integer exponent values, so use a
+  # custom function instead.
+  # REF: https://stackoverflow.com/a/32030190
+  defp pow(_n, 0), do: 1
+  defp pow(n, exp) when Integer.is_odd(exp), do: n * pow(n, exp - 1)
+  defp pow(n, exp) do
+    result = pow(n, div(exp, 2))
+    result * result
+  end
+end
