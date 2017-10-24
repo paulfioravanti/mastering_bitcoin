50f2c735b5b184903abdb297b21a085a3f00ed42
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-10-24 18:06:59 +1100

    Add stub for vanity miner module

---
+++ lib/mastering_bitcoin/vanity_miner.ex
@@ -0,0 +1,13 @@
+defmodule MasteringBitcoin.VanityMiner do
+  @moduledoc """
+  Example 4-9. Vanity address miner
+
+  NOTE: There isn't an Elixir library that wraps the C++ libbitcoin library,
+  and I couldn't figure out how to get Elixir to talk directly to libbitcoin,
+  and I couldn't seem to get the example addr.cpp file compiling in C++, so
+  this stays skipped for now.
+  """
+
+  def run do
+  end
+end
