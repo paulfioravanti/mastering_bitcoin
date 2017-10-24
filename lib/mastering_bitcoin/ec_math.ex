7b0ec478baabebc94bb2d76f6d4c4a72ebb732f4
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-10-24 18:01:33 +1100

    Add stub for ECMath module

---
+++ lib/mastering_bitcoin/ec_math.ex
@@ -0,0 +1,12 @@
+defmodule MasteringBitcoin.ECMath do
+  @moduledoc """
+  Example 4-7. A script demonstrating elliptic curve math used for bitcoin keys
+
+  NOTE: It seems that there isn't an Elixir wrapper or equivalent for
+  Elliptic Curve Digital Signature Algorithm (ECDSA), so leave this exercise
+  for now.
+  """
+
+  def run do
+  end
+end
