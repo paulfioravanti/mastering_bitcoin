0cc8205ab6659f2d79361662dc910b030357636c
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-12-08 16:08:18 +1100

    Complete and refactor solution for Addr

--- lib/mastering_bitcoin/addr.ex
+++ lib/mastering_bitcoin/addr.ex
@@ -4,32 +4,49 @@ defmodule MasteringBitcoin.Addr do
   private key.
 
   Currently a non-port over of the `addr.cpp` file.
+  Port over of some code contained in the `addr.cpp` file. Uses the following
+  libraries to get Elixir talking with C++:
+
+  Porcelain - Reaches out to the shell to compile C++ executable
+  Cure - Talks to the C++ code via Ports
   """
 
-  @src_compile """
-  g++ -std=c++11 -o priv/addr priv/addr.cpp \
+  @cpp_clean "rm priv/addr"
+  @cpp_compile """
+  g++ -std=c++11 -I./deps/cure/c_src -L./deps/cure/c_src -O3 -x c++ \
+  -o priv/addr priv/addr.cpp ./deps/cure/c_src/elixir_comm.c \
   $(pkg-config --cflags --libs libbitcoin)
   """
-  @src_execute "./priv/addr"
+  @cpp_executable "priv/addr"
   # Private secret key string as base16
   @private_key "038109007313a5807b2eccc082c8c3fbb988a973cacf1a7df9ce725c31b14776"
-  @generate_public_key 0
+
+  # Integers representing C++ methods
+  @generate_public_key 1
+  @create_bitcoin_address 2
 
   def run do
-    {:ok, server} = Cure.Server.start_link("./c_src/program")
-    public_key = generate_public_key(server)
-    IO.puts(public_key)
-    :ok = Cure.Server.stop(server)
-    # Porcelain.shell(@src_compile)
-
-    # @src_execute
-    # |> Porcelain.shell()
-    # |> Map.fetch!(:out)
-    # |> IO.write()
+    Porcelain.shell(@cpp_clean)
+    Porcelain.shell(@cpp_compile)
+    with {:ok, pid} <- Cure.Server.start_link(@cpp_executable),
+         public_key <- generate_public_key(pid),
+         bitcoin_address <- create_bitcoin_address(pid, public_key) do
+      IO.puts("Public key: #{inspect(public_key)}")
+      IO.puts("Address: #{inspect(bitcoin_address)}")
+      :ok = Cure.Server.stop(pid)
+    end
   end
 
-  def generate_public_key(server) do
-    Cure.send_data(server, <<@generate_public_key, @private_key>>, :once)
+  defp generate_public_key(pid) do
+    cure_data(pid, <<@generate_public_key, @private_key>>)
+  end
+
+  defp create_bitcoin_address(pid, public_key) do
+    cure_data(pid, <<@create_bitcoin_address, public_key :: binary>>)
+  end
+
+  defp cure_data(pid, data) do
+    Cure.send_data(pid, data, :once)
     receive do
       {:cure_data, response} ->
         response
--- priv/addr.cpp
+++ priv/addr.cpp
@@ -1,47 +1,90 @@
 #include <bitcoin/bitcoin.hpp>
+#include "addr.h"
 
-int main()
-{
-    // Private secret key string as base16
-    bc::ec_secret decoded;
-    bc::decode_base16(decoded,
-        "038109007313a5807b2eccc082c8c3fbb988a973cacf1a7df9ce725c31b14776");
+int main(void) {
+  int bytes_read;
+  byte buffer[MAX_BUFFER_SIZE];
 
-    bc::wallet::ec_private secret(
-        decoded, bc::wallet::ec_private::mainnet_p2kh);
+  while ((bytes_read = read_msg(buffer)) > 0) {
+    process_command(buffer, bytes_read);
+  }
 
-    // Get public key.
-    bc::wallet::ec_public public_key(secret);
-    std::cout << "Public key: " << public_key.encoded() << std::endl;
+  return 0;
+}
 
-    // Create Bitcoin address.
-    // Normally you can use:
-    //    bc::wallet::payment_address payaddr =
-    //        public_key.to_payment_address(
-    //            bc::wallet::ec_public::mainnet_p2kh);
-    //  const std::string address = payaddr.encoded();
+// Process the command dependent on the integer value given in the message
+// sent from Elixir
+void process_command(byte* buffer, int bytes_read) {
+  int function = buffer[0];
+  char arg[1024];
+  get_string_arg(buffer, arg, bytes_read);
+  std::string retval;
 
-    // Compute hash of public key for P2PKH address.
-    bc::data_chunk public_key_data;
-    public_key.to_data(public_key_data);
-    const auto hash = bc::bitcoin_short_hash(public_key_data);
+  if (bytes_read > 0) {
+    switch (function) {
+      case 1: {  // generate_public_key
+        retval = generate_public_key(buffer);
+        break;
+      } case 2: { // create_bitcoin_address
+        retval = create_bitcoin_address(buffer);
+        break;
+      } default: {
+        fprintf(stderr, "not a valid function %i\n", function);
+        exit(1);
+      }
+    }
+    memcpy(buffer, retval.data(), retval.length());
+    send_msg(buffer, retval.size());
+  } else {
+    fprintf(stderr, "no command given");
+    exit(1);
+  }
+}
 
-    bc::data_chunk unencoded_address;
-    // Reserve 25 bytes
-    //   [ version:1  ]
-    //   [ hash:20    ]
-    //   [ checksum:4 ]
-    unencoded_address.reserve(25);
-    // Version byte, 0 is normal BTC address (P2PKH).
-    unencoded_address.push_back(0);
-    // Hash data
-    bc::extend_data(unencoded_address, hash);
-    // Checksum is computed by hashing data, and adding 4 bytes from hash.
-    bc::append_checksum(unencoded_address);
-    // Finally we must encode the result in Bitcoin's base58 encoding.
-    assert(unencoded_address.size() == 25);
-    const std::string address = bc::encode_base58(unencoded_address);
+void get_string_arg(byte* buffer, char* string, int bytes_read) {
+  buffer[bytes_read] = '\0';
+  strcpy(string, (char*) &buffer[1]);
+}
 
-    std::cout << "Address: " << address << std::endl;
-    return 0;
+std::string generate_public_key(std::string priv_key) {
+  bc::ec_secret decoded;
+  bc::decode_base16(decoded, priv_key);
+
+  bc::wallet::ec_private secret(decoded, bc::wallet::ec_private::mainnet_p2kh);
+
+  // Get public key.
+  bc::wallet::ec_public public_key(secret);
+  return public_key.encoded();
+}
+
+std::string create_bitcoin_address(std::string pub_key) {
+  // Create Bitcoin address.
+  // Normally you can use:
+  //    bc::wallet::payment_address payaddr =
+  //        public_key.to_payment_address(
+  //            bc::wallet::ec_public::mainnet_p2kh);
+  //  const std::string address = payaddr.encoded();
+
+  bc::wallet::ec_public public_key = bc::wallet::ec_public::ec_public(pub_key);
+  // Compute hash of public key for P2PKH address.
+  bc::data_chunk public_key_data;
+  public_key.to_data(public_key_data);
+  const auto hash = bc::bitcoin_short_hash(public_key_data);
+
+  bc::data_chunk unencoded_address;
+  // Reserve 25 bytes
+  //   [ version:1  ]
+  //   [ hash:20    ]
+  //   [ checksum:4 ]
+  unencoded_address.reserve(25);
+  // Version byte, 0 is normal BTC address (P2PKH).
+  unencoded_address.push_back(0);
+  // Hash data
+  bc::extend_data(unencoded_address, hash);
+  // Checksum is computed by hashing data, and adding 4 bytes from hash.
+  bc::append_checksum(unencoded_address);
+  // Finally we must encode the result in Bitcoin's base58 encoding.
+  assert(unencoded_address.size() == 25);
+  const std::string address = bc::encode_base58(unencoded_address);
+  return address;
 }
