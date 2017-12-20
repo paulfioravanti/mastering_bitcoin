cbde5353118d74690a30dc3fd316156ea8784b06
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-12-20 12:16:39 +1100

    Refactor Chapter 4 tests

--- test/mastering_bitcoin/chapter_4_test.exs
+++ test/mastering_bitcoin/chapter_4_test.exs
@@ -2,33 +2,54 @@ defmodule Chapter4Test do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  setup(%{module: module}) do
-    result = capture_io(fn -> module.run() end)
-    {:ok, [result: result]}
+  describe "MasteringBitcoin.EllipticCurvePointCheck.run()" do
+    setup do
+      result = MasteringBitcoin.EllipticCurvePointCheck.run()
+      {:ok, [result: result, expected: 0]}
+    end
+
+    test "Example 4-1", %{result: result, expected: expected} do
+      assert result == expected
+    end
   end
 
-  @tag module: MasteringBitcoin.EllipticCurvePointCheck
-  test "Example 4-1", %{result: result} do
-    assert result
+  @tag lang: :cpp
+  describe "MasteringBitcoin.Addr.run()" do
+    setup do
+      result = capture_io(fn -> MasteringBitcoin.Addr.run() end)
+      expected =
+        """
+        Public key: \
+        "0202a406624211f2abbdc68da3df929f938c3399dd79fac1b51b0e4ad1d26a47aa"
+        Address: "1PRTTaJesdNovgne6Ehcdu1fpEdX7913CK"
+        """
+      {:ok, [result: result, expected: expected]}
+    end
+
+    test "Example 4-3", %{result: result, expected: expected} do
+      assert result == expected
+    end
   end
 
-  @tag module: MasteringBitcoin.Addr, lang: :cpp
-  test "Example 4-3", %{result: result} do
-    assert result
-  end
+  describe "Non-idempotent tests that output to stdout" do
+    setup(%{module: module}) do
+      result = capture_io(fn -> module.run() end)
+      {:ok, [result: result]}
+    end
 
-  @tag module: MasteringBitcoin.KeyToAddressECCExample, lang: :py
-  test "Example 4-5", %{result: result} do
-    assert result
-  end
+    @tag module: MasteringBitcoin.KeyToAddressECCExample, lang: :py
+    test "Example 4-5", %{result: result} do
+      assert result
+    end
 
-  @tag module: MasteringBitcoin.ECMath, lang: :py
-  test "Example 4-7", %{result: result} do
-    assert result
-  end
+    @tag module: MasteringBitcoin.ECMath, lang: :py
+    test "Example 4-7", %{result: result} do
+      assert result
+    end
 
-  @tag module: MasteringBitcoin.VanityMiner, lang: :cpp
-  test "Example 4-9", %{result: result} do
-    assert result
+    @tag module: MasteringBitcoin.VanityMiner, lang: :cpp
+    test "Example 4-9", %{result: result} do
+      assert result
+    end
   end
 end
